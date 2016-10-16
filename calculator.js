bf7da6368119d6ee6052f95d3ad1d0a8c45e0ad9
Jen J Wong <jenjwong@gmail.com>
2016-10-15 21:01:51 -0700

    Adds CSS styling

--- calculator.js
+++ calculator.js
@@ -1,58 +1,92 @@
-let input = '';
-let last = -1;
-
-const updateDisplay = function(val, wipe) {
-  const display = document.querySelector('.display');
-  if (wipe) {
-    display.textContent = val;
-    return
+class Input {
+  constructor() {
+    this.value = '';
+    this.history = [];
+    this.wasCalc = false;
   }
-  display.textContent += val;
-}
-
-const validInput = function(node){
-  node = node || document.body;
-  let classListArray = Array.from(node.classList);
-  console.log(last === 1 && input.length % 2 === 0, last, input.lenth%2)
-
-
-  if(input.slice(input.length) === 1 && input.length % 2 === 0 || classListArray.indexOf('operator') === 1 && last === 1) {
-    last = classListArray.indexOf('operator')
-    updateDisplay('', true);
-    updateDisplay('error');
-    setTimeout(function(){
-      console.log('called')
-      clear();
-    }, 1000);
-    return false
+  update(val) {
+    this.value += val;
+    return this.value;
   }
-  last = classListArray.indexOf('operator')
-  return true;
-}
-
-const keyPress = function() {
-  // if (!validInput(this)) {
-  //   return
-  // }
-
-  input+=this.textContent.trim()
-  updateDisplay(this.textContent)
-}
-
-const calculate = function() {
-  if (!validInput()) {
-    return;
+  set(val) {
+    this.value = val;
+    return this.value;
   }
-
-  result = eval(input);
-  updateDisplay(result, true);
+  clear() {
+    this.value = '';
+    this.history = [];
+  }
+  storeHistory(node) {
+    this.history.push(node);
+  }
 }
 
-const clear = function() {
-  updateDisplay('', true)
+const input = new Input;
+
+
+const clear = function () {
+  updateDisplay('');
+  input.clear();
 };
 
-(function(){
+const keyWasOperator = function(node) {
+  let currentNode;
+  if (node) {
+    currentNode = node;
+  } else {
+    currentNode = input.history[input.history.length - 1] || document.body;
+  }
+  const classListArrayCurrent = Array.from(currentNode.classList);
+  return classListArrayCurrent.indexOf('operator') === 1;
+};
+
+const validInput = function () {
+  if (input.history.length < 2 || keyWasOperator()) {
+    displayError();
+    return false;
+  }
+  return true;
+};
+
+const updateDisplay = function (val) {
+  const display = document.querySelector('.display');
+  display.textContent = '';
+  display.textContent = val;
+};
+
+const keyPress = function () {
+  if (input.wasCalc && !keyWasOperator(this)) {
+    clear();
+  }
+  input.storeHistory(this);
+  input.update(this.textContent.trim());
+  updateDisplay(input.value);
+  input.lastKey = this;
+  input.wasCalc = false;
+};
+
+const calculate = function () {
+  input.wasCalc = true;
+  let result;
+  if (!validInput()) {
+    input.clear();
+    return;
+  }
+  result = eval(input.value);
+  input.set(result);
+  updateDisplay(result);
+};
+
+const displayError = function () {
+  updateDisplay('error');
+  setTimeout(function () {
+    input.clear();
+    updateDisplay('');
+  }, 2000);
+};
+
+
+(function () {
   const keys = Array.from(document.querySelectorAll('.input'));
   const calculateButton = document.querySelector('.calculate');
   const clearButton = document.querySelector('.clear');
@@ -60,15 +94,13 @@ const clear = function() {
   calculateButton.addEventListener('click', calculate, false);
   clearButton.addEventListener('click', clear, false);
 
-  for(var i = 0; i < keys.length; i++) {
+  for (let i = 0; i < keys.length; i++) {
     keys[i].addEventListener('click', keyPress, false);
   }
-}())
+}());
 
-
-
-
-
+// Basic calculator algorithm that doesn't use eval method.
+// Doesn't currently handle order of opperations.
 
 // const calculate = function() {
 //   let result = input[0];
